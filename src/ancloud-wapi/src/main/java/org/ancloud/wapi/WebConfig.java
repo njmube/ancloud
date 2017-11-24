7f738781335157f4eea1aa9e652baaba37181a82
Tom <tamanle23@gmail.com>
2017-11-25 02:06:27 +0800

    Clean

--- src/ancloud-wapi/src/main/java/org/ancloud/wapi/WebConfig.java
+++ src/ancloud-wapi/src/main/java/org/ancloud/wapi/WebConfig.java
@@ -1,16 +1,19 @@
 package org.ancloud.wapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
-		@Bean
-        public CookieSerializer cookieSerializer() {
-                DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-                serializer.setCookieName("SESSIONID"); 
-                serializer.setCookiePath("/"); 
-                serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$"); 
-                return serializer;
-        }
+  @Bean
+  public CookieSerializer cookieSerializer() {
+    DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+    serializer.setCookieName("SESSIONID"); 
+    serializer.setCookiePath("/"); 
+    serializer.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$"); 
+    return serializer;
+  }
 }
